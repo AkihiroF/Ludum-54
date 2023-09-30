01cc4b7f6b2444ec05383d1d21cfcf231355c7d7
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2023-09-30 17:38:48 +0300

    Prototype UI and add TMP

---
+++ Assets/_Source/GameUISystem/GameUI.cs
@@ -0,0 +1,46 @@
+﻿namespace GameUISystem
+{
+    public class GameUI
+    {
+        private readonly GameUIView _view;
+        
+        public GameUI(GameUIView view)
+        {
+            _view = view;
+        }
+        
+        public void LookOnItem()
+        {
+            _view.HintEnable();
+        }
+
+        public void NotLookOnItem()
+        {
+            _view.HintDisable();
+        }
+        
+        public void AddEvent()
+        {
+            // _continueButton.onClick.AddListener(Continue);
+            // _settingsButton.onClick.AddListener(OpenSettings);
+            // _backButton.onClick.AddListener(CloseSettings);
+            // _exitButton.onClick.AddListener(Exit);
+            //
+            // Signals.Get<CloseEyeSignal>().AddListener(CloseEye);
+            //
+            // _view.OnOpenEye += EnablePlayerInput;
+        }
+
+        public void RemoveEvent()
+        {
+            // _continueButton.onClick.RemoveListener(Continue);
+            // _settingsButton.onClick.RemoveListener(OpenSettings);
+            // _backButton.onClick.RemoveListener(CloseSettings);
+            // _exitButton.onClick.RemoveListener(Exit);
+            //
+            // Signals.Get<CloseEyeSignal>().RemoveListener(CloseEye);
+            //
+            // _view.OnOpenEye -= EnablePlayerInput;
+        }
+    }
+}
---
+++ Assets/_Source/GameUISystem/GameUIView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace GameUISystem
+{
+    public class GameUIView : MonoBehaviour
+    {
+        [SerializeField] private GameObject hint;
+        
+        public void HintEnable()
+        {
+            hint.SetActive(true);
+        }
+
+        public void HintDisable()
+        {
+            hint.SetActive(false);
+        }
+    }
+}
