de1ac9d9b0cf67263bb4b88ecedb4cdb343bb0f3
Kulikov Valery <valenok801@gmail.com>
2023-10-02 16:53:56 +0300

    FixBugs

--- Assets/_Source/Player/PlayerScalerComponent.cs
+++ Assets/_Source/Player/PlayerScalerComponent.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using SoundSystem;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Zenject;
 
 namespace Player
@@ -13,21 +14,25 @@ namespace Player
         [SerializeField] private AudioSource downSize;
 
         private ISound _sound;
+        private InputAction _interactionAction;
 
         [Inject]
-        private void Construct(ISound sound)
+        private void Construct(ISound sound, PlayerInput input)
         {
             _sound = sound;
+            _interactionAction = input.PlayerActions.Interaction;
         }
         
         public void SetObjectScale(float scale)
         {
             var source = objectScaling.localScale.x > scale ? downSize : upSize;
+            _interactionAction.Disable();
             
             _sound.Play(source);
             objectScaling.DOScale(new Vector3(scale, scale, scale), timeScaling).OnComplete(() =>
             {
                 _sound.Stop(source);
+                _interactionAction.Enable();
             });
         }
     }
